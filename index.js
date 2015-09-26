f79835e9613c8e4321a43b04565464ca73ee45de
alex <alexandre.bintz@gmail.com>
2015-09-26 18:34:42 +0200

    add file comment

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/**
+ * Arnold - advanced media center based on VLC and NW.js
+ *
+ * index.js - Main file
+ *
+ * @author Alexandre Bintz <alexandre@bintz.io>
+ * 09/2015
+ */
+
+'use strict';
 
 var app = new App();
 app.start();
