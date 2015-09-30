667a3f3a7ef03824664f8a9f27c0d109e7c9b4dd
alex <alexandre.bintz@gmail.com>
2015-09-30 18:48:36 +0200

    player architecture review

--- app_modules/player.js
+++ app_modules/player.js
@@ -9,22 +9,28 @@
 
 'use strict';
 
-var util = require('util');
+var util         = require('util');
 var EventEmitter = require('events');
 
 /**
  * Player
  * inherits EventEmitter
  *
- * @param canvas { HTMLCanvasElement } - canvas to use to render frames
+ * @param root { HTMLElement } - root element to find player components in
  */
-function Player(canvas) {
+function Player(root) {
   EventEmitter.call(this);
+
+  // get ui elements
+
+  this.uiRoot         = root;
+  this.uiCanvas       = root.querySelector('.playerCanvas');
+  this.uiSubtitlesBox = root.querySelector('.playerSubtitlesBox');
 
   // init WebChimera renderer
 
-  var wcjs = require("./node_modules_hacked/wcjs-renderer");
-  this.vlc = wcjs.init(canvas);
+  var wcjs = require("../node_modules_hacked/wcjs-renderer");
+  this.vlc = wcjs.init(this.uiCanvas);
 
   // map WebChimera callbacks to EventEmitter events for convenience
 
@@ -39,198 +45,160 @@ function Player(canvas) {
   this.vlc.onTimeChanged = (function(time) {
     this.emit('timeChanged', time);
   }).bind(this);
+
+  // init subtitles engine
+
+  this.updateSubtitles = function(){};
+  this.on('timeChanged', (function(time) {
+    this.updateSubtitles(time/1000);
+  }).bind(this));
 }
 util.inherits(Player, EventEmitter);
 
-/**
- * Player - toggle Play/Pause
- */
-Player.prototype.togglePause = function () {
-
-  this.vlc.togglePause();
-}
+module.exports = Player;
 
 /**
- * Player - stop
- */
-Player.prototype.stop = function () {
-
-  this.vlc.stop();
-}
-
-/**
- * Player - jump
- *
- * @param delta { number } - jump delta in ms (can be negative for backward jump)
- */
-Player.prototype.jump = function (delta) {
-
-  this.vlc.time = this.vlc.time + delta;
-}
-
-/**
- * Player - set active audio track
- *
- * @param track { number }
- */
-Player.prototype.setAudioTrack = function (track) {
-
-  this.vlc.audio.track = track;
-}
-
-/**
- * Player - set active subtitles track
- *
- * @param track { number }
- */
-Player.prototype.setSubtitlesTrack = function (track) {
-
-  this.vlc.subtitles.track = track;
-}
-
-/**
- * Player - return current item in the playlist
- *
  * @return { WebChimera.VlcMedia }
  */
 Player.prototype.getCurrentPlaylistItem = function () {
-
   var index = this.vlc.playlist.currentItem;
   return this.vlc.playlist.items[index];
 }
 
 /**
- * Player - return current audio properties
- *
  * @return { WebChimera.VlcAudio }
  */
 Player.prototype.getAudio = function () {
-
   return this.vlc.audio;
 }
 
 /**
- * Player - return current audio track number
- *
  * @return { number }
  */
 Player.prototype.getCurrentAudioTrack = function () {
-
   return this.getAudio().track;
 }
 
 /**
- * Player - return current subtitles properties
- *
  * @return { WebChimera.VlcSubtitles }
  */
 Player.prototype.getSubtitles = function () {
-
   return this.vlc.subtitles;
 }
 
 /**
- * Player - return current subtitles track number
- *
  * @return { number }
  */
 Player.prototype.getCurrentSubtitlesTrack = function () {
-
   return this.getSubtitles().track;
 }
 
 /**
- * Player - play given URI
+ * App - Respond to command
  *
- * @param uri { string } - URI to play, can be :
- *                         • any kind of path or URL to any file that VLC would play
- *                           e.g. /User/john/file.mkv
- *                                http://example.com/file.avi
- *                         • local path to a .torrent file,
- *                           e.g. /User/john/file.torrent
- *                         • magnet link
- *                           e.g. magnet:?xt=urn:btih:72F242DB89E763B6CE390F25D576195C2169B149&dn=big+buck+bunny+4k+uhd+hfr+60fps+flac+webdl+2160p+x264+zmachine&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce
+ * @param cmd  { string } - command identifier
+ * @param args { array  } - command arguments
+ *
+ * Supported commands : • play(uri)
+ *                      • play
+ *                      • pause
+ *                      • togglePause
+ *                      • stop
+ *                      • jump(delta in ms)
+ *                      • setAudioTrack(track number)
+ *                      • toggleMute
+ *                      • setSubtitlesTrack(track number)
+ *                      • setSubtitlesFile(path or url)
  */
-Player.prototype.playUri = function(uri) {
+Player.prototype.cmd = function (cmd) {
 
-  if (this.isMagnet(uri) || this.isTorrent(uri)) {
-    this.playTorrent(uri);
+  var args = Array.prototype.slice.call(arguments);
+  args.shift();
 
-  } else {
-    this.playFile(uri);
+  if(cmd == 'play') {
+    if(args[0]) { var uri = args[0];
+
+      if (uri.startsWith('magnet:') || uri.endsWith('.torrent')) {
+
+        require('../app_modules/torrent').createStreamFromTorrent(uri, (function(err, stream) {
+          if(err) return;
+          this.cmd('play', stream.url);
+        }).bind(this));
+
+      } else {
+
+        if (uri.startsWith('http://')) {
+          this.vlc.play(uri);
+
+        } else {
+          this.vlc.play(uri.startsWith('file://') ? uri : 'file://'+uri);
+        }
+      }
+
+    } else {
+      this.vlc.play();
+    }
+  } else if(cmd == 'pause') {
+    this.vlc.pause();
+
+  } else if(cmd == 'togglePause') {
+    this.vlc.togglePause();
+
+  } else if(cmd == 'stop') {
+    this.vlc.stop();
+
+  } else if(cmd == 'jump') {
+    this.vlc.time = this.vlc.time + args[0];
+
+  } else if(cmd == 'setAudioTrack') {
+    this.vlc.audio.track = args[0];
+
+  } else if(cmd == 'toggleMute') {
+    this.vlc.toggleMute();
+
+  } else if(cmd == 'setSubtitlesTrack') {
+    this.vlc.subtitles.track = args[0];
+
+  } else if (cmd == 'setSubtitlesFile') {
+
+    if (args[0]) { var uri = args[0];
+
+      if (uri.startsWith('http://')) {
+
+        var dir = '/tmp/arnold/srt';
+        var name = new Date().getTime() + '.srt';
+        var path = dir+'/'+name;
+
+        new (require('download'))()
+        .get(uri)
+        .dest(dir)
+        .rename(name)
+        .run((function() {
+          this.cmd('setSubtitlesFile', path);
+        }).bind(this));
+
+      } else {
+
+        require('fs').readFile(uri, 'utf-8', (function(err, data) {
+          if(err) return;
+          this.updateSubtitles = (require('subplay'))(data, (function(text) {
+            this.writeSubtitle(text);
+          }).bind(this));
+        }).bind(this));
+      }
+
+    } else {
+
+      this.updateSubtitles(-1);
+      this.updateSubtitles = function(){};
+      this.writeSubtitle('');
+    }
   }
 }
 
 /**
- * Player - play magnet link or torrent file
- *
- * @param source { string } - magnet link
- *                            • e.g. magnet:?xt=urn:btih:72F242DB89E763B6CE390F25D576195C2169B149&dn=big+buck+bunny+4k+uhd+hfr+60fps+flac+webdl+2160p+x264+zmachine&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce
- *                          - local path to a .torrent file
- *                            • e.g. /User/john/file.torrent
+ * Write given subtitle item on screen
  */
-Player.prototype.playTorrent = function(source) {
-
-  require('./app_modules/torrent').createStreamFromTorrent(source, (function(err, stream) {
-    if(err) return;
-    this.playUri(stream.url);
-    app.mediaInfo.filepath = stream.filepath;
-  }).bind(this));
-}
-
-/**
- * Player - play media file
- *
- * @param uri { string } - any kind of path or URL to any file that VLC would play
- *                         e.g. /User/john/file.mkv
- *                              http://example.com/file.avi
- */
-Player.prototype.playFile = function(uri) {
-
-  if (this.isHTTP(uri)) {
-    this.playMRL(uri);
-
-  } else {
-    this.playMRL(uri.startsWith('file://') ? uri : 'file://'+uri);
-  }
-}
-
-/**
- * Player - play MRL
- *
- * @param mrl { string } - MRL that VLC can play
- */
-Player.prototype.playMRL = function(mrl) {
-
-  this.vlc.play(mrl);
-}
-
-/**
- * Player - test if URI is HTTP URL
- *
- * @param uri { string } - URI to test
- */
-Player.prototype.isHTTP = function(uri) {
-
-  return new RegExp('^http://').test(uri);
-}
-
-/**
- * Player - test if URI is magnet link
- *
- * @param uri { string } - URI to test
- */
-Player.prototype.isMagnet = function(uri) {
-
-  return new RegExp('^magnet').test(uri);
-}
-
-/**
- * Player - test if URI is .torrent file
- *
- * @param uri { string } - URI to test
- */
-Player.prototype.isTorrent = function(uri) {
-
-  return new RegExp('\.torrent$').test(uri)
+Player.prototype.writeSubtitle = function (text) {
+  this.uiSubtitlesBox.innerHTML = text;
 }
